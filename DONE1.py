1e6f12d8a9500a679e4e18bab6faaea6ed9620da
ekroll <ekroll@pm.me>
2024-02-22 13:08:00 +0100

    cleaned up code

--- DONE1.py
+++ DONE1.py
@@ -1,40 +1,33 @@
-import numpy as np
-import matplotlib as plt
-import matplotlib.pyplot as plt
-import math
-from PIL import Image
-
-
-# loads the image into array with one channel
-img = np.asarray(Image.open('data/image001.png', mode='r').convert('L'))
-# Copies image data to fx and fy
-img1 = img.copy()
-img2 = img.copy()
-
-dim = np.shape(img)
-c = 0.4 #0.45 rimelig verdi
 
 # Oppgave 1a
 # Derivert ved x
 def f_discrete_x(img):
     diff = img
-    for ix, iy in np.ndindex((img.shape[0] - 1, img.shape[1] - 1)):
-        diff[ix][iy] = (img[ix+1][iy] - img[ix][iy]) / 2.0 #gange med delta?
+
+    dsx = img.shape[0] - 1
+    dsy = img.shape[1] - 1
+    shape = np.ndindex((dsx, dsy))
+
+    for ix, iy in shape:
+        diff[ix][iy] = (img[ix+1][iy] - img[ix][iy]) / 2.0  # gange med delta?
 
     return diff
 
-fx = f_discrete_x(img1)
 
 # Oppgave 1b
 # Derivert ved y
-def f_discrete_y(mat):
-    diff = mat
-    for ix, iy in np.ndindex((mat.shape[0] - 1, mat.shape[1] - 1)):
-        diff[ix][iy] = (mat[ix][iy+1] - mat[ix][iy]) / 2.0 #gange med delta?
+def f_discrete_y(img):
+    diff = img
+
+    dsx = img.shape[0] - 1
+    dsy = img.shape[1] - 1
+    shape = np.ndindex((dsx, dsy))
+
+    for ix, iy in shape:
+        diff[ix][iy] = (img[ix][iy+1] - img[ix][iy]) / 2.0  # gange med delta?
 
     return diff
 
-fy = f_discrete_y(img2)
 
 # Oppgave 1c
 # Lengde av gradienten
@@ -44,15 +37,9 @@ def f_gradlen(dx, dy):
         diff[iy][ix] = np.sqrt((dx[iy][ix]) ** 2 + (dy[iy][ix]) ** 2)
     return diff
 
-fgradabs = f_gradlen(fx, fy)
-
-# Oppgave 1 d.
-maxval = fgradabs.max()
-minval = fgradabs.min()
-fgradabs = np.interp(fgradabs,(minval,maxval),(0,1))
 
 # Oppgave 1f
-def kutt(x, tolerance):
+def cut(x, tolerance):
     for ix, iy in np.ndindex(x.shape):
         if x[ix][iy] <= tolerance:
             x[ix][iy] = 0.0
@@ -60,8 +47,32 @@ def kutt(x, tolerance):
             x[ix][iy] = 1.0
     return x
 
-fgradabs = kutt(fgradabs, c)
 
-imgplot = plt.imshow(fgradabs, cmap='gist_gray')
+def main():
+    # loads the image into array with one channel
+    img = np.asarray(Image.open('data/image001.png', mode='r').convert('L'))
 
-plt.show()
+    # Copies image data to fx and fy
+    img1 = img.copy()
+    img2 = img.copy()
+
+    c = 0.4  # 0.45 rimelig verdi
+    fx = f_discrete_x(img1)
+    fy = f_discrete_y(img2)
+    fgradabs = f_gradlen(fx, fy)
+
+    # Oppgave 1 d.
+    (maxval, minval) = (fgradabs.max(), fgradabs.min())
+    fgradabs = np.interp(fgradabs, (minval, maxval), (0, 1))
+    fgradabs = cut(fgradabs, c)
+
+    # Vis bilde
+    plt.imshow(fgradabs, cmap='gist_gray')
+    plt.show()
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from PIL import Image
+    main()
